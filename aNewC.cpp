2cad8d8b9d405c962ca321160a8378bbf044dc9d
tagzmon <gabrielcphone@gmail.com>
2015-11-21 17:53:52 -0800

    added the new file to the cpp

---
+++ aNewC.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x;
+	cout<<"enter in a number for me please"<<endl;
+	cin>>x;
+	cout<<"the number you entered was "<<x<<endl;
+	return 0;
+}
